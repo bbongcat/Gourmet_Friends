edfd57a8027a2e0848cf5d487d3a9006a26fbc2d
yj <lollolllo328@gmail.com>
2021-04-21 11:48:00 +0900

    init commit

---
+++ src/main/java/com/gourmetfriends/gourmet_friends_prj/GourmetFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.gourmetfriends.gourmet_friends_prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetFriendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GourmetFriendsApplication.class, args);
+    }
+
+}
